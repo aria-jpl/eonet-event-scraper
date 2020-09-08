67016737c08f46cdb8e362b3c6b91f9a9d6090d3
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2020-09-08 12:07:49 -0700

    Incorporated updates from live version which are presumably important

--- rolling_update_wrapper.sh
+++ rolling_update_wrapper.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# This is placed in the directory referred to in the following example crontab entry
+# 00 * * * * source /export/home/hysdsops/verdi/bin/activate && /export/home/hysdsops/aria_cron_jobs/eonet/rolling_update_wrapper.sh 2>&1
+
 set -e
 
 # Define constants
-max_eonet_curation_delay="7"
+max_eonet_curation_delay="15"
 whole_world_polygon="[[-180,-90],[-180,90],[180,90],[180,-90],[-180,-90]]"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-${DIR}/submit_eonet_query.py --lookback_days ${max_eonet_curation_delay} --polygon ${whole_world_polygon}
+LD_LIBRARY_PATH=$HOME/conda/lib:/usr/lib:/usr/lib64:/usr/local/lib:$LD_LIBRARY_PATH ${DIR}/submit_eonet_query.py --lookback_days ${max_eonet_curation_delay} --polygon ${whole_world_polygon}
