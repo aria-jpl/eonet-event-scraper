4ef2b194435e77725792c46e434b02fda1984e88
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2020-09-09 12:25:05 -0700

    Resolved weird merge issue where these commits weren't pulled in PR

--- rolling_update_wrapper.sh
+++ rolling_update_wrapper.sh
@@ -8,8 +8,9 @@ branch=${1:-"main"}
 set -e
 
 # Define constants
+queue="edunn-jplnet-dev"
 max_eonet_curation_delay="15"
 whole_world_polygon="[[-180,-90],[-180,90],[180,90],[180,-90],[-180,-90]]"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LD_LIBRARY_PATH=$HOME/conda/lib:/usr/lib:/usr/lib64:/usr/local/lib:$LD_LIBRARY_PATH ${DIR}/submit_eonet_query.py --lookback_days ${max_eonet_curation_delay} --polygon ${whole_world_polygon} --version $branch
+LD_LIBRARY_PATH=$HOME/conda/lib:/usr/lib:/usr/lib64:/usr/local/lib:$LD_LIBRARY_PATH ${DIR}/submit_eonet_query.py --lookback_days ${max_eonet_curation_delay} --polygon ${whole_world_polygon} --version $branch --queue $queue
